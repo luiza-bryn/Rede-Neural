e703dfafa9ac41d79988215556892500cb0e5a40
Filipe Ribeiro <liperrocha@gmail.com>
2025-06-22 15:15:53 -0300

    funcoes de erro e suas derivadas

--- neural_network_lib/loss.py
+++ neural_network_lib/loss.py
@@ -1,3 +1,39 @@
-class Loss:
-    def __init__(self):
-        pass
+import numpy as np
+
+# Erro Quadratico Medio (MSE)
+def  mean_squared_error(y_true, y_pred):
+    mse = np.mean((y_true - y_pred)**2)
+    return mse
+
+def mean_squared_error_derivative(y_true, y_pred):
+    mse_drivative = 2*(y_true - y_pred) / y_true.size
+    return mse_drivative
+
+# Entropia Cruzada Binaria
+def binary_cross_entropy(y_true, y_pred):
+    # Evitar o resultado de log(0)
+    epsilon = 1e-15
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    bce = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+    return bce
+
+def binary_cross_entropy_derivative(y_true, y_pred):
+    # Evitar o resultado de log(0)
+    epsilon = 1e-15
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    bce_derivative = -(y_true / y_pred) + ((1 - y_true) / (1 - y_pred))
+    return bce_derivative
+
+# Entropia Cruzada Categorica
+def categorical_cross_entropy(y_true, y_pred):
+    # Evitar o resultado de log(0)
+    epsilon = 1e-15
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    cce = -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
+    return cce
+
+def categorical_cross_entropy_derivative(y_true, y_pred):
+    # Evitar o resultado de log(0)
+    epsilon = 1e-15
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    cce_derivative = -(y_true / y_pred) / y_true.shape[0]
